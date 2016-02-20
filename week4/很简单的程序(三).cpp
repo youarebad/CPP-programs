17a6f969ce35ddf15916f66ed6f20968403209a0
youarebad <youarebad@foxmail.com>
2016-02-20 22:31:50 +0800

    yoooooo~~~~~~

---
+++ week4/很简单的程序(三).cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a = 0;
+	cout <<"请输入一个数"<< endl;
+	cin >> a;
+	cout << "我刚才输入的数:" << a << endl;
+	return 0;
+
+}
---
+++ week4/很简单的程序(二).cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a = 0;
+	cout <<"我想输出的数："<<a << endl;
+	return 0;
+
+}
